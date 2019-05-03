e4b3847621e0fdb1f65c08f7e860c825c144fd3e
Vinayak.S.Karande <38550519+vinayakkarande@users.noreply.github.com>
2019-05-03 09:50:59 +0530

    ok
    ok

--- Code/models.py
+++ Code/models.py
@@ -6,6 +6,7 @@ from keras.models import Model, Sequential, load_model
 from keras.layers import Conv2D, MaxPooling2D, Input, Conv1D, MaxPooling3D, Conv3D, ConvLSTM2D, LSTM, AveragePooling2D
 from keras import optimizers
 from keras.callbacks import EarlyStopping
+import sys
 
 class Model_List():
     def __init__(self, model, frames, dimensions, saved_model=None, print_model=False):
@@ -21,26 +22,20 @@ class Model_List():
         if self.saved_model is not None:
             print("Loading model %s" % self.saved_model)
             self.model = load_model(self.saved_model)
-        
-        elif model == 'CNN':
+        elif model == 'CNN_AVG_reg':
             print("Loading Model.")
-            self.model = self.CNN()
-        elif model == 'CNN_DROPOUT':
+            self.model = self.CNN_AVG_reg()
+        elif model == 'CNN_MAX_reg':
             print("Loading Model.")
-            self.model = self.CNN_DROPOUT()
-        elif model == 'CNN_AVG_small':
+            self.model = self.CNN_MAX_reg()
+        elif model == 'CNN_LSTM_AVG':
             print("Loading Model.")
-            self.model = self.CNN_AVG_small()
-        elif model == 'CNN_AVG':
-            print("Loading Model.")
-            self.model = self.CNN_AVG()
-        elif model == 'CNN_MAX':
-            print("Loading Model.")
-            self.model = self.CNN_MAX()
+            self.model = self.CNN_LSTM_AVG()
         
         elif model == 'CNN_LSTM':
             print("Loading Model.")
             self.model = self.CNN_LSTM()
+            
         elif model == 'SepCNN_LSTM':
             print("Loading Model.")
             self.model = self.SepCNN_LSTM()
@@ -59,94 +54,46 @@ class Model_List():
             print(self.model.summary())
                            
                            
-    def CNN(self):
+    def CNN_AVG_reg(self):
         #frames_input = Input(shape=self.input_shape)
         model = Sequential()
-        model.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(38, 256, 10)))
-        model.add(MaxPooling2D((2, 2)))
-        model.add(Conv2D(64, (3, 3), activation='relu'))
-        model.add(MaxPooling2D((2, 2)))
-        model.add(Flatten())
-        model.add(Dense(512, activation='relu'))
-        model.add(Dense(1))
-        return model
-    
-    def CNN_AVG(self):
-        #frames_input = Input(shape=self.input_shape)
-        model = Sequential()
-        model.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(38, 256, 10)))
+        model.add(Conv2D(32, (3, 3),kernel_regularizer=regularizers.l2(0.001), activation='relu', padding='same', input_shape=(38, 256, 10)))
         model.add(AveragePooling2D((2, 2)))
-        model.add(Conv2D(64, (3, 3), activation='relu'))
-        model.add(AveragePooling2D((2, 2)))
-        model.add(Conv2D(128, (3, 3), activation='relu'))
-        model.add(AveragePooling2D((2, 2)))
-        model.add(Conv2D(256, (3, 3), activation='relu'))
+        model.add(Conv2D(64, (3, 3),kernel_regularizer=regularizers.l2(0.001), activation='relu'))
         model.add(Flatten())
         model.add(Dropout(0.5))
-        model.add(Dense(512, activation='relu'))
+        model.add(Dense(64,kernel_regularizer=regularizers.l2(0.001), activation='relu'))
         model.add(Dense(1))
         return model
-    
-    def CNN_AVG_small(self):
+     
+    def CNN_MAX_reg(self):
         #frames_input = Input(shape=self.input_shape)
         model = Sequential()
-        model.add(Conv2D(32, (1, 2), activation='relu', padding='same', input_shape=(38, 256, 10)))
+        model.add(Conv2D(32, (3, 3),kernel_regularizer=regularizers.l2(0.001),activation='relu', padding='same', input_shape=(38, 256, 10)))
+        model.add(MaxPooling2D((2, 2)))
+        model.add(Conv2D(64, (3, 3),kernel_regularizer=regularizers.l2(0.001), activation='relu'))
+        model.add(Flatten())
+        model.add(Dropout(0.5))
+        model.add(Dense(64,kernel_regularizer=regularizers.l2(0.001), activation='relu'))
+        model.add(Dense(1))
+        return model 
+    
+    def CNN_LSTM_AVG(self):
+
+        input_frames = Input(shape=self.input_shape)
+        model = Sequential()
+        model.add(Conv2D(32, (3, 3),kernel_regularizer=regularizers.l2(0.001), activation='relu', padding='same', input_shape=self.image_dim))
         model.add(AveragePooling2D((2, 2)))
-        model.add(Conv2D(64, (1, 2), activation='relu'))
-        model.add(AveragePooling2D((2, 2)))
-        model.add(Conv2D(128, (1, 2), activation='relu'))
-        model.add(AveragePooling2D((2, 2)))
-        model.add(Conv2D(128, (1, 2), activation='relu'))
+        model.add(Conv2D(64, (3, 3),kernel_regularizer=regularizers.l2(0.001), activation='relu'))
         model.add(Flatten())
-        model.add(Dropout(0.5))
-        model.add(Dense(512, activation='relu'))
-        model.add(Dense(1))
-        return model
-    
-    
-    def CNN_MAX(self):
-        #frames_input = Input(shape=self.input_shape)
-        model = Sequential()
-        model.add(Conv2D(32, (1, 2), activation='relu', padding='same', input_shape=(38, 256, 10)))
-        model.add(MaxPooling2D((2, 2)))
-        model.add(Conv2D(64, (1, 2), activation='relu'))
-        model.add(MaxPooling2D((2, 2)))
-        model.add(Conv2D(128, (1, 2), activation='relu'))
-        model.add(MaxPooling2D((2, 2)))
-        model.add(Conv2D(128, (1, 2), activation='relu'))
-        model.add(Flatten())
-        model.add(Dropout(0.5))
-        model.add(Dense(512, activation='relu'))
-        model.add(Dense(1))
-        return model
-    
-    
-    def CNN_DROPOUT1(self):
-        #frames_input = Input(shape=self.input_shape)
-        model = Sequential()
-        model.add(Conv2D(32, (2, 2), activation='relu', padding='same', input_shape=(38, 256, 10)))
-        model.add(MaxPooling2D((2, 2)))
-        model.add(Conv2D(64, (2, 2), activation='relu'))
-        model.add(MaxPooling2D((2, 2)))
-        model.add(Flatten())
-        model.add(Dropout(0.5))
-        model.add(Dense(512, activation='relu'))
-        model.add(Dense(1))
-        return model
-    
-    def CNN_DROPOUT(self):
-        #frames_input = Input(shape=self.input_shape)
-        model = Sequential()
-        model.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(38, 256, 10)))
-        model.add(MaxPooling2D((2, 2)))
-        model.add(Conv2D(64, (3, 3), activation='relu'))
-        model.add(MaxPooling2D((2, 2)))
-        model.add(Flatten())
-        model.add(Dropout(0.5))
-        model.add(Dense(512, activation='relu'))
-        model.add(Dense(1))
-        return model
-    
+        encoded_frame_sequence = TimeDistributed(model)(frames_input) # the output will be a sequence of vectors
+        encoded_video = LSTM(256, activation='tanh', return_sequences=True)(encoded_frame_sequence)  # the output will be a vector
+        fc2 = Dense(64, activation='relu', kernel_regularizer=regularizers.l2(0.001))(encoded_video)
+        out = Flatten()(fc2)
+        out = Dropout(0.5)(out)
+        output = Dense(1, activation='relu')(out)
+        CNN_LSTM_AVG = Model(inputs=input_frames, outputs=output)
+        return CNN_LSTM_AVG
     
     def CNN_LSTM(self):
 
